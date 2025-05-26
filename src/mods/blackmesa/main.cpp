786207fa6b3a9b784b560506fe476f67507c9294
#xoxor4d <r4d0xzz@live.de>
2025-05-26 19:55:19 +0200

    lots of FF

--- src/mods/blackmesa/main.cpp
+++ src/mods/blackmesa/main.cpp
@@ -115,7 +115,8 @@ BOOL APIENTRY DllMain(HMODULE hmodule, const DWORD ul_reason_for_call, LPVOID)
 			return TRUE;
 		}
 
-		shared::common::loader::module_loader::register_module(std::make_unique<mods::blackmesa::d3d9ex>());
+		// asi gets loaded afterwards
+		//shared::common::loader::module_loader::register_module(std::make_unique<mods::blackmesa::d3d9ex>());
 
 		if (const auto t = CreateThread(nullptr, 0, mods::blackmesa::find_game_window_by_sha1, nullptr, 0, nullptr); t) {
 			CloseHandle(t);
--- src/mods/blackmesa/modules/renderer.cpp
+++ src/mods/blackmesa/modules/renderer.cpp
@@ -2,6 +2,7 @@
 #include "renderer.hpp"
 
 #include "shared/common/flags.hpp"
+#include "shared/common/remix.hpp"
 
 namespace mods::blackmesa
 {
@@ -103,14 +104,233 @@ namespace mods::blackmesa
 			set_remix_emissive_intensity(dev, ctx, 0.0f);
 		}*/
 
-		// shader: VertexLitGeneric (infected - player model - viewmodel - dynamic props)
-		// > models/weapons/melee/crowbar
-		// > models/props_junk/wood_palletcrate001a
-		// shader: Refract_DX90
-		// > vgui/hud/scope_sniper_ul
-		if (mesh->m_VertexFormat == 0xa0003)
-		{
+		// shader: GBFast
+		// > __gbufferfast_brush00
+		// > __gbfastpropwrite000
+
+		if (ctx.info.shader_name == "GBFast") {
+			ctx.modifiers.do_not_render = true;
 		}
+		else if (ctx.info.shader_name == "GBLight") {
+			ctx.modifiers.do_not_render = true;
+		}
+
+		else if (   mesh->m_VertexFormat == 0x80103
+			|| mesh->m_VertexFormat == 0x82087) 
+		{
+			ctx.modifiers.do_not_render = false;
+		}
+
+		// WriteZ_DX9
+		// > engine/writez
+		// screenspace_general_dx9
+		// > dev/lumcompare
+		// Engine_Post_dx9
+		// > dev/engine_post_nxtgen
+		else if (mesh->m_VertexFormat == 0x80101)
+		{
+
+			ctx.modifiers.do_not_render = false;
+
+			// FIRST "UI/HUD" elem (remix injection triggers here)
+				// -> fullscreen color transitions (damage etc.) and also "enables" the crosshair
+			if (ctx.info.shader_name.starts_with("Engine_")) // Engine_Post
+			{
+				// do not fog HUD elements :D
+				dev->SetRenderState(D3DRS_FOGENABLE, FALSE);
+
+				// yep this is bad .. todo: view+0x3C to 0x48 = m_FadeColorRGBA
+				DWORD* view = reinterpret_cast<DWORD*>(*(DWORD*)(CLIENT_BASE + 0x5390A0));
+				void*** g_ViewEffects = reinterpret_cast<void***>(view);
+				char r, g, b, a;
+				char blend;
+				((void(__thiscall*)(void***, char*, char*, char*, char*, char*))(*g_ViewEffects)[2])(g_ViewEffects, &r, &g, &b, &a, &blend);
+
+				float rr = (float)r * 0.0039215689f;
+				float gg = (float)g * 0.0039215689f;
+				float bb = (float)b * 0.0039215689f;
+				float aa = (float)a * 0.0039215689f;
+				//Vector4D
+				/*const auto s_viewFadeModulate = reinterpret_cast<bool*>(CLIENT_BASE + USE_OFFSET(0x0, 0x9ECEE0));
+				if (s_viewFadeModulate && *s_viewFadeModulate)
+				{ }*/
+
+				ctx.save_vs(dev);
+				dev->SetVertexShader(nullptr);
+				dev->SetPixelShader(nullptr); // needed
+
+				ctx.save_texture(dev, 0);
+				dev->SetTexture(0, nullptr); // disable bound texture
+
+				ctx.save_rs(dev, D3DRS_ALPHABLENDENABLE);
+				dev->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
+
+				ctx.save_rs(dev, D3DRS_BLENDOP);
+				dev->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_ADD);
+
+				ctx.save_rs(dev, D3DRS_SRCBLEND);
+				dev->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+
+				ctx.save_rs(dev, D3DRS_DESTBLEND);
+				dev->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+
+				ctx.save_rs(dev, D3DRS_ZWRITEENABLE);
+				dev->SetRenderState(D3DRS_ZWRITEENABLE, FALSE);
+
+				ctx.save_rs(dev, D3DRS_ZENABLE);
+				dev->SetRenderState(D3DRS_ZENABLE, FALSE);
+
+				dev->SetTransform(D3DTS_WORLD, &ctx.info.buffer_state.m_Transform[0]);
+				dev->SetTransform(D3DTS_VIEW, &ctx.info.buffer_state.m_Transform[1]);
+				dev->SetTransform(D3DTS_PROJECTION, &ctx.info.buffer_state.m_Transform[2]);
+
+				struct CUSTOMVERTEX
+				{
+					float x, y, z, rhw;
+					D3DCOLOR color;
+				};
+
+				auto color = D3DCOLOR_COLORVALUE(rr, gg, bb, aa);
+				const auto w = (float)ctx.info.buffer_state.m_Viewport.Width + 0.5f;
+				const auto h = (float)ctx.info.buffer_state.m_Viewport.Height + 0.5f;
+
+				CUSTOMVERTEX vertices[] =
+				{
+					{ -0.5f, -0.5f, 0.0f, 1.0f, color }, // tl
+					{     w, -0.5f, 0.0f, 1.0f, color }, // tr
+					{ -0.5f,     h, 0.0f, 1.0f, color }, // bl
+					{     w,     h, 0.0f, 1.0f, color }  // br
+				};
+
+				dev->SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
+				dev->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, vertices, sizeof(CUSTOMVERTEX));
+
+				// do not render the original mesh
+				ctx.modifiers.do_not_render = true;
+			}
+		}
+
+		// LightmappedGeneric
+		// > metal/ibeam_blue
+		else if (  mesh->m_VertexFormat == 0x2480133
+				|| mesh->m_VertexFormat == 0x480103 /*decals*/
+				|| mesh->m_VertexFormat == 0x480107 /*decals*/)
+		{
+			//ctx.modifiers.do_not_render = true;
+
+			if (ctx.info.shader_name == "WorldVertexTransition_DX9")
+			{
+				ctx.save_texture(dev, 0); // helps with culling issue
+				ctx.modifiers.dual_render_with_basetexture2 = true;
+			}
+
+			// m_BoundTexture[7]  = first blend colormap
+			// m_BoundTexture[12] = second blend colormap
+
+			// if envmap		:: VERTEX_TANGENT_S | VERTEX_TANGENT_T | VERTEX_NORMAL is set
+			// if basetex2		:: vertex color is set
+			// if bumpmap		:: tc count = 3 ... else 2
+
+			// texcoord0 : base texcoord
+			// texcoord1 : lightmap texcoord
+			// texcoord2 : lightmap texcoord offset
+
+#if 0
+			IDirect3DVertexDeclaration9* vertex_decl = nullptr;
+			dev->GetVertexDeclaration(&vertex_decl);
+
+			enum d3ddecltype : BYTE
+			{
+				D3DDECLTYPE_FLOAT1 = 0,		// 1D float expanded to (value, 0., 0., 1.)
+				D3DDECLTYPE_FLOAT2 = 1,		// 2D float expanded to (value, value, 0., 1.)
+				D3DDECLTYPE_FLOAT3 = 2,		// 3D float expanded to (value, value, value, 1.)
+				D3DDECLTYPE_FLOAT4 = 3,		// 4D float
+				D3DDECLTYPE_D3DCOLOR = 4,	// 4D packed unsigned bytes mapped to 0. to 1. range
+
+				// Input is in D3DCOLOR format (ARGB) expanded to (R, G, B, A)
+				D3DDECLTYPE_UBYTE4 = 5,		// 4D unsigned byte
+				D3DDECLTYPE_SHORT2 = 6,		// 2D signed short expanded to (value, value, 0., 1.)
+				D3DDECLTYPE_SHORT4 = 7,		// 4D signed short
+
+				// The following types are valid only with vertex shaders >= 2.0
+				D3DDECLTYPE_UBYTE4N = 8,	// Each of 4 bytes is normalized by dividing to 255.0
+				D3DDECLTYPE_SHORT2N = 9,	// 2D signed short normalized (v[0]/32767.0,v[1]/32767.0,0,1)
+				D3DDECLTYPE_SHORT4N = 10,	// 4D signed short normalized (v[0]/32767.0,v[1]/32767.0,v[2]/32767.0,v[3]/32767.0)
+				D3DDECLTYPE_USHORT2N = 11,  // 2D unsigned short normalized (v[0]/65535.0,v[1]/65535.0,0,1)
+				D3DDECLTYPE_USHORT4N = 12,  // 4D unsigned short normalized (v[0]/65535.0,v[1]/65535.0,v[2]/65535.0,v[3]/65535.0)
+				D3DDECLTYPE_UDEC3 = 13,		// 3D unsigned 10 10 10 format expanded to (value, value, value, 1)
+				D3DDECLTYPE_DEC3N = 14,		// 3D signed 10 10 10 format normalized and expanded to (v[0]/511.0, v[1]/511.0, v[2]/511.0, 1)
+				D3DDECLTYPE_FLOAT16_2 = 15,	// Two 16-bit floating point values, expanded to (value, value, 0, 1)
+				D3DDECLTYPE_FLOAT16_4 = 16,	// Four 16-bit floating point values
+				D3DDECLTYPE_UNUSED = 17,	// When the type field in a decl is unused.
+			};
+			enum d3ddecluse : BYTE
+			{
+				D3DDECLUSAGE_POSITION = 0,
+				D3DDECLUSAGE_BLENDWEIGHT,   // 1
+				D3DDECLUSAGE_BLENDINDICES,  // 2
+				D3DDECLUSAGE_NORMAL,        // 3
+				D3DDECLUSAGE_PSIZE,         // 4
+				D3DDECLUSAGE_TEXCOORD,      // 5
+				D3DDECLUSAGE_TANGENT,       // 6
+				D3DDECLUSAGE_BINORMAL,      // 7
+				D3DDECLUSAGE_TESSFACTOR,    // 8
+				D3DDECLUSAGE_POSITIONT,     // 9
+				D3DDECLUSAGE_COLOR,         // 10
+				D3DDECLUSAGE_FOG,           // 11
+				D3DDECLUSAGE_DEPTH,         // 12
+				D3DDECLUSAGE_SAMPLE,        // 13
+			};
+			struct d3dvertelem
+			{
+				WORD Stream;		// Stream index
+				WORD Offset;		// Offset in the stream in bytes
+				d3ddecltype Type;	// Data type
+				BYTE Method;		// Processing method
+				d3ddecluse Usage;	// Semantics
+				BYTE UsageIndex;	// Semantic index
+			};
+
+			d3dvertelem decl[MAX_FVF_DECL_SIZE]; UINT numElements = 0;
+			vertex_decl->GetDeclaration((D3DVERTEXELEMENT9*)decl, &numElements);
+			int x = 1;
+#endif
+			ctx.save_vs(dev); 
+			dev->SetVertexShader(nullptr);
+		}
+
+		// also renders text 
+		// UnlitGeneric
+		// > console/background01_widescreen
+		else if (mesh->m_VertexFormat == 0x80107)
+		{
+			//ctx.save_vs(dev);
+			//dev->SetVertexShader(nullptr);
+		}
+
+		else if (mesh->m_VertexFormat == 0xa0003)
+		{
+			// viewmodel
+			if (ctx.info.buffer_state.m_Transform[2].m[3][2] == -1.00003338f)
+			{
+				//ctx.save_view_transform(dev);
+				//ctx.save_projection_transform(dev);
+				//dev->SetTransform(D3DTS_VIEW, &ctx.info.buffer_state.m_Transform[1]);
+				//dev->SetTransform(D3DTS_PROJECTION, &ctx.info.buffer_state.m_Transform[2]);
+			}
+
+			ctx.save_vs(dev);
+			dev->SetVertexShader(nullptr);
+		}
+
+		else 
+		{
+			auto yy = 0; 
+		}
+
+
+		/*auto x = mesh->m_VertexFormat; 
+		auto y = 0;*/
 	}
 
 
@@ -133,12 +353,198 @@ namespace mods::blackmesa
 		}
 	}
 
+
+
+	//void cmeshdx8_renderpass_post_draw(std::uint32_t num_prims_rendered)
+	void cmeshdx8_renderpass_post_draw([[maybe_unused]] void* device_ptr, D3DPRIMITIVETYPE type, std::int32_t base_vert_index, std::uint32_t min_vert_index, std::uint32_t num_verts, std::uint32_t start_index, std::uint32_t prim_count)
+	{
+		const auto dev = game::get_d3d_device();
+		const auto shaderapi = game::get_shaderapi();
+		auto& ctx = renderer::primctx;
+
+		// 0 = Gamma 1.0 (fixes dark albedo) :: 1 = Gamma 2.2
+		dev->SetSamplerState(0, D3DSAMP_SRGBTEXTURE, ctx.modifiers.with_high_gamma ? 1u : 0u);
+
+		// do not render next surface if set
+		if (!ctx.modifiers.do_not_render)
+		{
+			if (ctx.modifiers.og_mesh_z_offset != 0.0f)
+			{
+				ctx.info.buffer_state.m_Transform[0].m[3][2] += ctx.modifiers.og_mesh_z_offset;
+				dev->SetTransform(D3DTS_WORLD, &ctx.info.buffer_state.m_Transform[0]);
+			}
+
+			dev->DrawIndexedPrimitive(type, base_vert_index, min_vert_index, num_verts, start_index, prim_count);
+
+			// restore transform
+			if (ctx.modifiers.og_mesh_z_offset != 0.0f)
+			{
+				ctx.info.buffer_state.m_Transform[0].m[3][2] -= ctx.modifiers.og_mesh_z_offset;
+				dev->SetTransform(D3DTS_WORLD, &ctx.info.buffer_state.m_Transform[0]);
+			}
+		}
+
+		// render the current surface a second time (alpha blended) if set
+		// only works with shaders using basemap2 in sampler7
+		if (ctx.modifiers.dual_render_with_basetexture2)
+		{
+			// check if basemap2 is assigned
+			if (ctx.info.buffer_state.m_BoundTexture[7])
+			{
+				// save texture, renderstates and texturestates
+
+				IDirect3DBaseTexture9* og_tex0 = nullptr;
+				dev->GetTexture(0, &og_tex0);
+
+				DWORD og_alphablend = {};
+				dev->GetRenderState(D3DRS_ALPHABLENDENABLE, &og_alphablend);
+
+				DWORD og_alphaop = {}, og_alphaarg1 = {}, og_alphaarg2 = {};
+				dev->GetTextureStageState(0, D3DTSS_ALPHAOP, &og_alphaop);
+				dev->GetTextureStageState(0, D3DTSS_ALPHAARG1, &og_alphaarg1);
+				dev->GetTextureStageState(0, D3DTSS_ALPHAARG2, &og_alphaarg2);
+
+				DWORD og_colorop = {}, og_colorarg1 = {}, og_colorarg2 = {};
+				dev->GetTextureStageState(0, D3DTSS_COLOROP, &og_colorop);
+				dev->GetTextureStageState(0, D3DTSS_COLORARG1, &og_colorarg1);
+				dev->GetTextureStageState(0, D3DTSS_COLORARG2, &og_colorarg2);
+
+				DWORD og_srcblend = {}, og_destblend = {};
+				dev->GetRenderState(D3DRS_SRCBLEND, &og_srcblend);
+				dev->GetRenderState(D3DRS_DESTBLEND, &og_destblend);
+
+
+				// assign basemap2 to textureslot 0
+				if (const auto basemap2 = shaderapi->vtbl->GetD3DTexture(shaderapi, nullptr, ctx.info.buffer_state.m_BoundTexture[7]);
+					basemap2)
+				{
+					dev->SetTexture(0, basemap2);
+				}
+
+				// enable blending
+				dev->SetRenderState(D3DRS_ALPHABLENDENABLE, 1);
+
+				// picking up / moving a cube affects this and causes flickering on the blended surface
+				dev->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+				dev->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+
+				// can be used to lighten up the albedo and add a little more alpha
+				dev->SetRenderState(D3DRS_TEXTUREFACTOR, D3DCOLOR_RGBA(0, 0, 0, 30));
+				dev->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+				dev->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_TFACTOR);
+				dev->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_ADD);
+
+				// add a little more alpha
+				dev->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_DIFFUSE);
+				dev->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_TFACTOR);
+				dev->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_ADD);
+
+				//ctx.info.buffer_state.m_Transform[0].m[3][2] += 0.05f;
+				dev->SetTransform(D3DTS_WORLD, &ctx.info.buffer_state.m_Transform[0]);
+
+				// draw second surface 
+				dev->DrawIndexedPrimitive(type, base_vert_index, min_vert_index, num_verts, start_index, prim_count);
+
+				// restore texture, renderstates and texturestates
+				dev->SetTexture(0, og_tex0);
+				dev->SetRenderState(D3DRS_ALPHABLENDENABLE, og_alphablend);
+				dev->SetRenderState(D3DRS_SRCBLEND, og_srcblend);
+				dev->SetRenderState(D3DRS_DESTBLEND, og_destblend);
+				dev->SetTextureStageState(0, D3DTSS_ALPHAARG1, og_alphaarg1);
+				dev->SetTextureStageState(0, D3DTSS_ALPHAARG2, og_alphaarg2);
+				dev->SetTextureStageState(0, D3DTSS_ALPHAOP, og_alphaop);
+				dev->SetTextureStageState(0, D3DTSS_COLORARG1, og_colorarg1);
+				dev->SetTextureStageState(0, D3DTSS_COLORARG2, og_colorarg2);
+				dev->SetTextureStageState(0, D3DTSS_COLOROP, og_colorop);
+			}
+		}
+
+		if (ctx.modifiers.dual_render_with_specified_texture)
+		{
+			// save og texture
+			IDirect3DBaseTexture9* og_tex0 = nullptr;
+			dev->GetTexture(0, &og_tex0);
+
+			// set new texture
+			dev->SetTexture(0, ctx.modifiers.dual_render_texture);
+
+			// BLEND ADD mode
+			if (ctx.modifiers.dual_render_with_specified_texture_blend_add)
+			{
+				ctx.save_rs(dev, D3DRS_ALPHABLENDENABLE);
+				dev->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
+
+				ctx.save_rs(dev, D3DRS_BLENDOP);
+				dev->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_ADD);
+
+				ctx.save_rs(dev, D3DRS_SRCBLEND);
+				dev->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_ONE);
+
+				ctx.save_rs(dev, D3DRS_DESTBLEND);
+				dev->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_ONE);
+
+				ctx.save_rs(dev, D3DRS_ZWRITEENABLE);
+				dev->SetRenderState(D3DRS_ZWRITEENABLE, FALSE);
+
+				ctx.save_rs(dev, D3DRS_ZENABLE);
+				dev->SetRenderState(D3DRS_ZENABLE, FALSE);
+
+				shared::common::remix::set_texture_category(dev, REMIXAPI_INSTANCE_CATEGORY_BIT_WORLD_MATTE | REMIXAPI_INSTANCE_CATEGORY_BIT_IGNORE_OPACITY_MICROMAP);
+			}
+
+			if (ctx.modifiers.dual_render_texture_z_offset != 0.0f)
+			{
+				ctx.info.buffer_state.m_Transform[0].m[3][2] += ctx.modifiers.dual_render_texture_z_offset;
+				dev->SetTransform(D3DTS_WORLD, &ctx.info.buffer_state.m_Transform[0]);
+			}
+
+			if (ctx.modifiers.as_water)
+			{
+				shared::common::remix::set_texture_hash(dev, shared::utils::string_hash32(ctx.info.material_name));
+			}
+
+			// re-draw surface
+			dev->DrawIndexedPrimitive(type, base_vert_index, min_vert_index, num_verts, start_index, prim_count);
+
+			// restore texture
+			dev->SetTexture(0, og_tex0);
+		}
+
+		//add_light_to_texture_color_restore();
+
+		// reset prim/pass modifications
+		renderer::primctx.restore_all(dev);
+		renderer::primctx.reset_context();
+		dev->SetFVF(NULL);
+	}
+
+	HOOK_RETN_PLACE_DEF(cmeshdx8_renderpass_post_draw_retn_addr);
+	void __declspec(naked) cmeshdx8_renderpass_post_draw_stub()
+	{
+		__asm
+		{
+			// og code
+			mov     ecx, [eax];
+			push    dword ptr[edi + 0x50];
+			push    dword ptr[edi + 0x44];
+			push    eax;
+			call	cmeshdx8_renderpass_post_draw; // instead of 'edx' (DrawIndexedPrimitive)
+			add		esp, 0x1C;
+			jmp		cmeshdx8_renderpass_post_draw_retn_addr;
+		}
+	}
+
+
+
 	renderer::renderer()
 	{
 		p_this = this;
 
 		shared::utils::hook(RENDERER_BASE + 0x3C5E7, cmeshdx8_renderpass_pre_draw_stub, HOOK_JUMP).install()->quick();
 		HOOK_RETN_PLACE(cmeshdx8_renderpass_pre_draw_retn_addr, RENDERER_BASE + 0x3C5EC);
+
+		shared::utils::hook(RENDERER_BASE + 0x3C718, cmeshdx8_renderpass_post_draw_stub, HOOK_JUMP).install()->quick();
+		HOOK_RETN_PLACE(cmeshdx8_renderpass_post_draw_retn_addr, RENDERER_BASE + 0x3C727);
 
 		m_initialized = true;
 		std::cout << "[RENDERER] loaded\n";
